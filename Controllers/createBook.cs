a63c966e1b1112a76664f3d9f646e6d4264c24f8
quxly4 <quely004@gmail.com>
2024-04-20 11:06:36 -0600

    controlador

---
+++ Controllers/createBook.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2019AC603_2021CO601.Controllers
+{
+    public class createBook : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
