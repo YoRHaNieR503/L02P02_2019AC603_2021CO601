d4dd11421977df3552a15957535fd7bff855a3a2
YoRHaNieR503 <christpoher.alarcon@catolica.edu.sv>
2024-04-20 11:31:31 -0600

    creacion de viewData

---
+++ Controllers/createBookController.cs
@@ -0,0 +1,32 @@
+﻿using L02P02_2019AC603_2021CO601.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace L02P02_2019AC603_2021CO601.Controllers
+{
+    public class createBookController : Controller
+    {
+        private readonly libreriaDbContext _dbContext;
+
+        public createBookController(libreriaDbContext libreriaDbContexto)
+        {
+            _dbContext = libreriaDbContexto;
+        }
+
+        public IActionResult Index()
+        {
+
+/***********************************************************************************************************/
+            var listaAutor = (from a in _dbContext.autores
+                              select a).ToList();
+            ViewData["listaAutores"] = new SelectList(listaAutor, "id", "nombre");
+/***********************************************************************************************************/
+            var listaCategoria = (from c in _dbContext.categorias
+                                  select c).ToList();
+            ViewData["listaCategoria"] = new SelectList(listaAutor, "id", "categoria");
+/***********************************************************************************************************/
+
+            return View();
+        }
+    }
+}
