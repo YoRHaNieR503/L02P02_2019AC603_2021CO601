0e7ed45c85a4fd16a94764899429ef9f1d9ee8d5
YoRHaNieR503 <christpoher.alarcon@catolica.edu.sv>
2024-04-20 10:27:26 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2019AC603_2021CO601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
